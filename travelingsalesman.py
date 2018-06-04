e9ee85f11e46d97fe9e101be677d0e97f97103f2
jessemckenna <20506673+jessemckenna@users.noreply.github.com>
2018-06-04 15:03:29 -0700

    Block out order of functions needed

--- travelingsalesman.py
+++ travelingsalesman.py
@@ -1,4 +1,6 @@
 '''
+Algorithm reference: http://cs.indstate.edu/cpothineni/alg.pdf
+
 Requirements:
 - Accept problem instances on the command line
 - Name the output file as the input file's name with .tour appended (for example 
@@ -25,23 +27,45 @@ Output specifications:
 
 import sys
 
-if len(sys.argv) != 2:
-	print("Usage: python travelingsalesman.py inputfilename")
-else:
-	inFile = sys.argv[1] # first argument
-	outFile = inFile + ".tour" # ex. input.txt -> input.txt.tour
-	output = ""
+def rowMin():
+	return
 
-	with open(inFile) as f: # inFile is open in this block and auto-closed after
-		read_data = f.read() # get entire file content as string
+def colMin():
+	return
 
-	cities = {} # to hold cities, key: city identifier, value: [x, y]
+def rowReduction():
+	return
 
-	arr = read_data.split('\n') # split content into lines
-	for i in range(len(arr)): # for each line
-		if len(arr[i]) > 0:
-			arr[i] = arr[i].split() # split into [identifier, x, y]
-			cities[arr[i][0]] = arr[i][1:3] # add to dict
+def colReduction():
+	return
 
-	with open(outFile, "w") as f:
-		f.write(output)
+# Calculate the bounds
+def checkBounds():
+	return
+
+# Execute branch and bound algorithm; called "main" in the reference
+def branchAndBound():
+	return
+
+# Read input, call branch and bound function, write output
+def main():
+	if len(sys.argv) != 2:
+		print("Usage: python travelingsalesman.py inputfilename")
+	else:
+		inFile = sys.argv[1] # first argument
+		outFile = inFile + ".tour" # ex. input.txt -> input.txt.tour
+		output = ""
+
+		with open(inFile) as f: # inFile is open in this block and auto-closed after
+			read_data = f.read() # get entire file content as string
+
+		cities = {} # to hold cities, key: city identifier, value: [x, y]
+
+		arr = read_data.split('\n') # split content into lines
+		for i in range(len(arr)): # for each line
+			if len(arr[i]) > 0:
+				arr[i] = arr[i].split() # split into [identifier, x, y]
+				cities[arr[i][0]] = arr[i][1:3] # add to dict
+
+		with open(outFile, "w") as f:
+			f.write(output)
